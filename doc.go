73d83207bd6f3be8ad56c0df91ecfbd3aa0d7ca3
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2015-05-07 21:42:46 +0200

    Update docs.

--- doc.go
+++ doc.go
@@ -1,2 +1,5 @@
 // Package schema helps to migrate the schema of a MySQL database.
+// 
+// The context all functions receive has to be initialized before using the package
+// https://github.com/altipla-consulting/database
 package schema
